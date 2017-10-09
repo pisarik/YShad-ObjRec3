98a5d7f5bb07f690b8ff01cc75ff0aed675ea553
Pisarik <mr_plum@mail.ru>
2017-10-09 04:57:59 +0300

    more dropout

--- multi_cls/tiles_with_overlap/cnn/cnn_model.py
+++ multi_cls/tiles_with_overlap/cnn/cnn_model.py
@@ -91,14 +91,14 @@ if __name__ == '__main__':
     # model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.25))
+    model.add(Dropout(0.25))
 
     model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
     # model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     model.add(Dense(111, activation='softmax'))
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
